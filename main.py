e630cf5c3f541f599d58dc6c5c2222e6bcd9e248
vinsis <6437295+vinsis@users.noreply.github.com>
2018-07-09 15:38:59 +0800

    miscellaneous changes

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from utils import all_models, optimizers, device
 from utils import criterion, dataloader, save_boundary_labels
 from utils import save_obj, load_obj
 
-def train(epoch):
+def train(epoch, model=None):
+    models_to_train = all_models if model is None else [model]
     for i, (x,y) in enumerate(dataloader):
         y = y.unsqueeze(-1).float()
         x = x.to(device)
         y = y.to(device)
-        for model in all_models:
+        for model in models_to_train:
             optimizer = optimizers[model.name]
             optimizer.zero_grad()
             y_pred = model(x)
@@ -18,6 +19,8 @@ def train(epoch):
                 print('Epoch: [{}], Iteration: [{}], Model: [{}], Loss: [{}]'.format(epoch, i, model.name, loss.item()))
             if i % 4 == 0:
                 save_boundary_labels(model, epoch=epoch, iteration=i//4)
+            if i%1000 == 0 and model.name.startswith('residual_v2'):
+                print('multiplier: {}'.format(model.multiplier))
     return i
 
 if __name__ == '__main__':
